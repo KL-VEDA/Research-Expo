78e7c9a9188c7f85b49e7f59a25f4c22a6169770
kl-veda <veda@kluniversity.in>
2025-09-08 19:20:52 +0530

    Categories

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Navbar from './components/Navbar/Navbar.jsx'; // Import the Navbar
 import LandingPage from './pages/LandingPage/LandingPage';
 import About from './pages/About/About.jsx';  // Import About page
 import Winners from './pages/Winners/Winners.jsx';
+import Categories from './pages/Categories/Categories.jsx'
+
 import "./App.css";
 
 function App() {
@@ -13,8 +15,9 @@ function App() {
       <Navbar />  {/* Add Navbar here */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<About />} />  {/* Add route for About page */}
+        <Route path="/about" element={<About />} />  
         <Route path="/winners" element={<Winners/>}/>
+        <Route path="/categories" element={<Categories/>}/>
       </Routes>
     </Router>
   );
