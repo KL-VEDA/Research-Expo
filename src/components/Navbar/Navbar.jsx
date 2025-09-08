910fe0232db407518479a5baf1d71f7bf6acd22f
kl-veda <veda@kluniversity.in>
2025-09-08 18:49:26 +0530

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,28 @@
-// ./components/Navbar/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import kllogo from '../../assets/kllogo.png';  // Ensure this is the correct path to the logo
-import './Navbar.css'; // Styles for Navbar
+import kllogo from '../../assets/kllogo.png';  
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className="navbar-container">
+    <nav className="navbar-container">
+      {/* Logo Section */}
       <div className="navbar-logo">
         <img src={kllogo} alt="KL Logo" className="navbar-logo-image" />
       </div>
+
+      {/* Navigation Links */}
       <div className="navbar-links">
         <Link to="/" className="navbar-link">Home</Link>
         <Link to="/about" className="navbar-link">About</Link>
-        <Link to="/register" className="navbar-link">Register</Link>
-        {/* <Link to="/contact" className="navbar-link">Contact</Link> */}
+        <Link to="/categories" className="navbar-link">Categories</Link>
+        <Link to="/rules" className="navbar-link">Rules</Link>
+        <Link to="/winners" className="navbar-link">Winners</Link>
+        <Link to="/register" className="navbar-link">Registration</Link>
+        <Link to="/media" className="navbar-link">Media</Link>
+        <Link to="/contact" className="navbar-link">Contact</Link>
       </div>
-    </div>
+    </nav>
   );
 }
 
